f6b2c6aeaa46b212fb1fe7fb82739f5e44f8df8a
Ahmad Syarifuddin <syarifuddinahmads.me@gmail.com>
2024-07-29 21:15:12 +0700

    test publish

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,9 +1,13 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   basePath:'/inisiasi-bumi',
+  output:"export",
   exported:true,
   plugins: {
     tailwindcss: {},
+  },
+  images: {
+    unoptimized: true,
   },
 };
 
