6a028e1b83dbdb811a97afbacf3f9266a6c24a50
Ahmad Syarifuddin <syarifuddinahmads.me@gmail.com>
2024-07-30 08:08:22 +0700

    update yaml

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,13 +1,8 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
-  basePath:'/inisiasi-bumi',
   output:"export",
-  exported:true,
   plugins: {
     tailwindcss: {},
-  },
-  images: {
-    unoptimized: true,
   },
 };
 
